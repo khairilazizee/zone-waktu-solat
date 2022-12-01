7a1aa5b55288d8fc40169a05d926d58902cfade7
Mohamad khairil azizee bin mohamed @ ayub <khairil114@gmail.com>
2022-12-01 15:33:07 +0800

    Update scrap_zone.py

--- scrap_zone.py
+++ scrap_zone.py
@@ -2,15 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 
 page = requests.get("https://www.e-solat.gov.my")
-soup = BeautifulSoup(page.content, "parser.html")
-# dropdown zone
+soup = BeautifulSoup(page.content, "html.parser")
+# search for dropdown zone
 results = soup.find(id="inputzone")
-zones = results.find("option", class_="hs")
+# print(results.prettify())
+zones = results.find_all("option", class_="hs")
 res_dict = {}
 for zone in zones:
-  # zone["value"]
-  for value in zone.stripped_strings:
-    res_dict[zone["value"]] = value
-             
-# to convert to json
-print(json.dumps(res_dict, indent=4)
+    for value in zone.stripped_strings:
+        res_dict[zone["value"]] = value
+
+# convert to json
+print(json.dumps(res_dict, indent=4))
